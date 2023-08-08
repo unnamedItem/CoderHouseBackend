3dfed1d71bd01a1d012bdce4fbb5f53c0baabd5d
fcusmai <fcusmai@b2gov.com>
2023-08-08 16:24:47 -0300

    -

--- p1/index.js
+++ p1/index.js
@@ -23,11 +23,21 @@ class ProductManager {
     }
 
     addProduct(product) {
-        this.productos.push(product)
+        const isAlreadyAdded = this.productos.some(prod => prod.code === product.code);
+        if (!isAlreadyAdded) {
+            this.productos.push(product);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     getProductList() {
         return this.productos;
+    }
+
+    getProductById(code) {
+        return this.productos.find(prod => prod.code === code);
     }
 }
 
@@ -35,7 +45,10 @@ let manager = new ProductManager();
 manager.addProduct(new Product("item1", 100, "This is a description", "1"));
 manager.addProduct(new Product("item2", 50, "This is a description", "2"));
 manager.addProduct(new Product("item3", 175, "This is a description", "3"));
+manager.addProduct(new Product("item3", 175, "This is a description", "2"));
 
 manager.getProductList().forEach(prod => {
-    console.log(`ID: ${prod.code} - $${prod.price} [${prod.title}] (${prod.stock})`)
+    console.log(`ID: ${prod.code} - $${prod.price} [${prod.title}] (${prod.stock})`);
 })
+
+console.log(manager.getProductById("2"));
